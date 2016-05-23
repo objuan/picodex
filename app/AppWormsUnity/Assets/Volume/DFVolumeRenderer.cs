2fda6d2a30f17ab1f52786281fd4a341e58db72f
marco cecchini <marcostress@gmail.com>
2016-05-23 21:07:13 +0200

    mesh

--- app/AppWormsUnity/Assets/Volume/DFVolumeRenderer.cs
+++ app/AppWormsUnity/Assets/Volume/DFVolumeRenderer.cs
@@ -7,172 +7,152 @@ using Picodex.Vxcm;
 
 namespace Picodex
 {
-    /// Controls some visual aspects of the volume and allows it to be rendered.
-    /**
-	 * The role of the VolumeRenderer component for volumes is conceptually similar to the role of Unity's MeshRenderer class for meshes.
-	 * Specifically, it can be attached to a GameObject which also has a Volume component to cause that Volume component to be drawn. It 
-	 * also exposes a number of properties such as whether a volume should cast and receive shadows.
-	 * 
-	 * Remember that Cubiquity acctually draws the volume by creating standard Mesh objects. Internally Cubiquity will copy the properties
-	 * of the VolumeRenderer to the MeshRenderers which are generated.
-	 * 
-	 * \sa VolumeCollider
-	 */
-    //  [ExecuteInEditMode]
+    public enum DFVolumeRendererMode
+    {
+        Raycast = 0,
+        Mesh
+    }
+ 
     [AddComponentMenu("Vxcm/DFVolumeRenderer")]
     [ExecuteInEditMode]
     public class DFVolumeRenderer : MonoBehaviour
     {
-        [System.NonSerialized]
-        Texture3D texture = null;
-
         [Range(0, 1)]
         public float CutPlaneXZ = 1f;
 
-        [System.NonSerialized]
-        Mesh proxyMesh;
+        public bool castShadows = true;
 
-        [SerializeField]
-        private Material mMaterial;
+        public bool receiveShadows = true;
 
-        [SerializeField]
-        private bool mReceiveShadows = true;
+        public DFVolumeRendererMode renderMode = DFVolumeRendererMode.Raycast;
 
-        [SerializeField]
-        private bool mShowWireframe = false;
-
-        /// \cond
-        [System.NonSerialized]
-        public bool hasChanged = true;
-        /// \endcond
+        // runtime
+        VXCMObject_v02 vxcmObject;
 
         DFVolume volume;
 
-        Matrix4x4 objectToVolumeTrx;
-
         [System.NonSerialized]
         public GameObject proxyGameObject;
+
+        [RangeAttribute(1,4)]
+        public int lod=1;
+
+        [System.NonSerialized]
+        Material _material;
 
         /// Material for this volume.
         public Material material
         {
             get
             {
-                return mMaterial;
+                if (renderMode == DFVolumeRendererMode.Raycast)
+                {
+                    _material = vxcmObject.GetComponent<Renderer>().material;
+                }
+                else
+                    _material = proxyGameObject.GetComponent<Renderer>().material;
+                return _material;
+            }
+            set
+            {
             }
         }
-      
-		/// Controls whether this volume casts shadows.
-		public bool castShadows
-		{
-			get
-			{
-				return mCastShadows;
-			}
-			set
-			{
-				if(mCastShadows != value)
-				{
-					mCastShadows = value;
-                    hasChanged = true;
-				}
-			}
-		}
-		[SerializeField]
-		private bool mCastShadows = true;
-		
-		/// Controls whether this volume receives shadows.
-		public bool receiveShadows
-		{
-			get
-			{
-				return mReceiveShadows;
-			}
-			set
-			{
-				if(mReceiveShadows != value)
-				{
-					mReceiveShadows = value;
-                    hasChanged = true;
-				}
-			}
-		}
-		
-        /// Controls whether the wireframe overlay is displayed when this volume is selected in the editor.
-        public bool showWireframe
-		{
-			get
-			{
-				return mShowWireframe;
-			}
-			set
-			{
-				if(mShowWireframe != value)
-				{
-					mShowWireframe = value;
-                    hasChanged = true;
-				}
-			}
-		}
 
-        // 
+ 
+ 
+        [System.NonSerialized]
+        public DFVolumeRendererMode old_renderMode = DFVolumeRendererMode.Raycast;
+
+        [System.NonSerialized]
+        public int old_lod =1;
 
 
         void Awake()
         {
 
-          //  Debug.Log("Awake 1");
+            //  Debug.Log("Awake 1");
             // proxy
-            proxyGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            DestroyImmediate(proxyGameObject.GetComponent<Collider>());
+            rebuild();
 
+        }
 
-
-
+        void rebuild()
+        {
             if (GetComponent<DFVolumeFilter>() == null) return;
             if (GetComponent<DFVolumeRenderer>() == null) return;
 
             volume = GetComponent<DFVolumeFilter>().volume;
 
-            // ---------------
 
-            VXCMObject_v02 obj = proxyGameObject.AddComponent<VXCMObject_v02>();
-            obj.volume = GetComponent<DFVolumeFilter>().volume;
+            if (gameObject.transform.childCount  > 0)
+            {
+                // detach the child
+                GameObject go = gameObject.transform.GetChild(0).gameObject;
+                go.transform.parent = null;
+                DestroyImmediate(go);
+                vxcmObject = null;
+            }
+            if (renderMode == DFVolumeRendererMode.Raycast)
+            {
+                proxyGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                DestroyImmediate(proxyGameObject.GetComponent<Collider>());
 
-            proxyGameObject.hideFlags = HideFlags.HideAndDontSave;
-           // proxyGameObject.hideFlags = HideFlags.DontSave;
+                // link on root
 
+                // ---------------
+
+                vxcmObject = proxyGameObject.AddComponent<VXCMObject_v02>();
+                vxcmObject.volume = GetComponent<DFVolumeFilter>().volume;
+
+
+                // proxyGameObject.hideFlags = HideFlags.DontSave;
+
+            }
+            else
+            {
+                proxyGameObject = VXCMMeshBuilder.CreateMeshTransvoxel(volume, new Vector3i(64, 64, 64), lod);
+            }
+
+            proxyGameObject.name = "VXCM Volume";
             proxyGameObject.transform.parent = this.transform;
             proxyGameObject.transform.setLocalToIdentity();
-        }
 
+            //proxyGameObject.hideFlags = HideFlags.HideAndDontSave;
+            proxyGameObject.hideFlags = HideFlags.DontSave;
+
+            // save
+            old_renderMode = renderMode;
+            old_lod = lod;
+        }
 
         void Start() {
 
-          ///  Debug.Log("Start 1");
-            //  volume = GetComponent<DFVolume>();
-
-            // mMaterial = new Material(Shader.Find("Vxcm/Object/ray_v05"));
-
-
+         //   material;
         }
 
-        void OnEnable()
+        void OnWillRenderObject()
         {
-            hasChanged = true;
+            //if (renderMode != old_renderMode || old_lod != lod)
+            //{
+            //    rebuild();
+            //}
         }
 
-        void OnDisable()
+#if UNITY_EDITOR
+        void Update()
         {
-            hasChanged = true;
+            // check for UI changes
+            if (renderMode != old_renderMode || old_lod != lod)
+            {
+                rebuild();
+            }
         }
-
+#endif
 
         void OnApplicationQuit()
         {
             DestroyImmediate(proxyGameObject);
         }
-
 
         // It seems that we need to implement this function in order to make the volume pickable in the editor.
         // It's actually the gizmo which get's picked which is often bigger than than the volume (unless all
@@ -183,71 +163,6 @@ namespace Picodex
             DFVolumeUI.OnDrawGizmos(gameObject);
         }
 
-       
-        //public void OnWillRenderObject()
-        //{
-        //    if (!enabled)// || !renderer || !renderer.sharedMaterial || !renderer.enabled)
-        //        return;
-
-        //    Camera cam = Camera.current;
-        //    if (!cam) return;
-
-        //    UpdateTexture();
-
-        //    UpdateMat();
-        //}
-
-        //public void UpdateTexture()
-        //{
-        //    if (texture == null || (texture != null && (texture.width != volume.resolution.x) || (texture.height != volume.resolution.y) || (texture.depth != volume.resolution.z)))
-        //    {
-        //        if (true)
-        //        {
-        //            VolumePrimitiveSphere raster = new VolumePrimitiveSphere(data.volume);
-
-        //            GeometrySample sample = new GeometrySample();
-        //            sample.debugColor = new Vector3(1, 0, 0);
-
-        //            raster.Raster(new Vector3(-10, 0, 0), 10, sample);
-        //            raster.Raster(new Vector3(10, 0, 0), 10, sample);
-        //        }
-
-        //        PrimitiveHelper.CreateCube(proxyMesh, volume.resolution.x, volume.resolution.y, volume.resolution.z);
-
-        //        texture = new Texture3D(volume.resolution.x, volume.resolution.y, volume.resolution.z, TextureFormat.RGBA32, false);
-        //        texture.SetPixels32(data.DF);
-        //        texture.Apply();
-
-        //        objectToVolumeTrx = new Matrix4x4();
-
-        //        Bounds bounds = proxyMesh.bounds;
-
-        //        float m = 1.0f / 2;
-        //        Vector3 scale = new Vector3(1.0f / volume.resolution.x, 1.0f / volume.resolution.y, 1.0f / volume.resolution.z);
-        //        objectToVolumeTrx.SetTRS(new Vector3(m, m, m), Quaternion.identity, scale);
-
-        //        // mat
-
-        //        material.SetFloat("DF_MIN", volume.data.distanceFieldRangeMin);
-        //        material.SetFloat("DF_MAX_MINUS_MIN", volume.data.distanceFieldRangeMax - volume.data.distanceFieldRangeMin);
-
-        //        material.SetMatrix("u_objectToVolumeTrx", objectToVolumeTrx);
-        //        material.SetMatrix("u_objectToVolumeInvTrx", objectToVolumeTrx.inverse);
-
-        //        material.SetVector("u_textureRes", scale);
-
-
-        //    }
-        //}
-
-        //public void UpdateMat()
-        //{
-        //    if (Camera.current == null) return;
-        //    if (material == null) return;
-
-        //    material.SetTexture("_Volume", texture);
-
-        //    material.SetFloat("u_cut_plane_xz", CutPlaneXZ);
-        //}
+   
     }
 }
